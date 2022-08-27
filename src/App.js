8e65713fa7f83efc3d131b01bd46ce635db5d9b6
AzraYazid <azrayazidalkautsar@gmail.com>
2022-08-27 16:16:34 +0700

    tambahan footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbaar from './components/navbar/navbar';
 import Header from './components/header/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footeer from './components/footer/footer';
 
 function App() {
   return (
     <>
       <Navbaar />
       <Header />
+      <Footeer />
     </>
   );
 }
